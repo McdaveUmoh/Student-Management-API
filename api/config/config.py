41b2ff5a6c7d50ffd2476424cea800ed44315d6c
Mcdave Umoh <umohmcdave@gmail.com>
2023-03-18 11:43:12 +0100

    config

--- api/config/config.py
+++ api/config/config.py
@@ -1,13 +1,15 @@
 import os
-import re
 from decouple import config
 from datetime import timedelta
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
+import os
+import re
+
 uri = config("DATABASE_URL") # or other relevant config var
 if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgressql://", 1)
+    uri = uri.replace("postgres://", "postgresql://", 1)
 #rest of connection code using the connection string `uri`
 
  
